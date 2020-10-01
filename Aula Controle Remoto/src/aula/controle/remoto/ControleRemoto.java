224be41eff24b118d4057f2ff5490006910206f6
Willian Sostenes <sostenes655@gmail.com>
2020-09-30 23:54:13 -0300

    Continuação do projeto

--- Aula Controle Remoto/src/aula/controle/remoto/ControleRemoto.java
+++ Aula Controle Remoto/src/aula/controle/remoto/ControleRemoto.java
@@ -9,6 +9,96 @@ package aula.controle.remoto;
  *
  * @author PORTOMASTER
  */
-public class ControleRemoto {
+public class ControleRemoto implements Controlador {
+    //ATRIBUTOS
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+   
+//MÉTODO CONSTRUTOR
+    public void ControleRemoto(){
+        volume = 50;
+        ligado = true;
+        tocando = false;
+    }
+    //MÉTODOS ESPECIAIS
+    public int getVolume(){
+        return this.volume;
+    }
+    public void setVolume(int volume){
+        this.volume = volume;
+    }
+    public boolean getLigado(){
+        return this.ligado;
+    }
+    public void setLigado(boolean ligado){
+        this.ligado = true;
+    }
+    public boolean getTocando(){
+        return this.tocando;
+    }
+    public void setTocando(boolean tocando){
+        this.tocando = false;
+    }
+    //MÉTODOS ABSTRATOS
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + this.getLigado());
+        System.out.println("Está tocando " + this.getTocando());
+        System.out.print("Volume: " + this.getVolume());
+        for (int i = 0; i<= this.getVolume(); i+=10){
+            System.out.println("|");
+        }
+        
+        
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu... ");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.getLigado()) 
+            this.setVolume(this.getVolume()+ 5);
+                
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.getLigado())
+            this.setVolume(this.getVolume() -5);
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.getLigado()&& )
+    }
+
+    @Override
+    public void descligarMudo() {
+    }
+
+    @Override
+    public void play() {
+    }
+
+    @Override
+    public void pause() {
+    }
     
 }
+
+
